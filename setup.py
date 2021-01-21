9c6a56bc6043c532916354b5088338ce886a34f5
Jacob Andrew Hester <j.andrew.hester@gmail.com>
2021-01-21 11:17:50 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Back of the napkin analysis of historical beef cattle prices in Alabama since 2004.',
+    author='Andrew Hester',
+    license='MIT',
+)
