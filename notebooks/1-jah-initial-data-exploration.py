62b86f77cdbb137053dddc56387c25ececb02b0a
Jacob Andrew Hester <j.andrew.hester@gmail.com>
2021-01-25 11:26:02 -0600

    added visual for overall sales per year

--- notebooks/1-jah-initial-data-exploration.py
+++ notebooks/1-jah-initial-data-exploration.py
@@ -8,6 +8,8 @@ Created on Fri Jan 22 13:11:45 2021
 import os
 import glob
 import pandas as pd 
+import altair as alt
+import altair_viewer
 
 # load in dataframes and concatenate together
 path = (
@@ -34,7 +36,28 @@ df['Hd_cnt'] = df['Hd_cnt'].str.replace(',', '')
 data_types_dictionary = {'Hd_cnt' : float}
 df = df.astype(data_types_dictionary)
 
-# things to look at:
-# 1 - overall sales per month over time 
+# check for missing values 
+df.isnull().sum()
+
+# look at overall sales per year 
+sales_per_yr_df = df.groupby(['Year'])['Hd_cnt'].sum()
+sales_per_yr_df = pd.DataFrame(sales_per_yr_df)
+sales_per_yr_df.reset_index(inplace = True)
+sales_per_yr_df.head()
+alt.renderers.enable('altair_viewer')
+bar = alt.Chart(sales_per_yr_df).mark_bar(size = 20).encode(
+    x = 'Year', 
+    y = alt.Y('Hd_cnt', axis = alt.Axis(title = 'Head Count'))
+).properties(
+        title = {
+            "text" : "Overall Sales Per Year",
+            "subtitle" : "Beef Cattle Sales in Alabama"}
+        )
+bar.show()
+
+# look at overall prices compared to sales per year 
+
+
+
 
 
